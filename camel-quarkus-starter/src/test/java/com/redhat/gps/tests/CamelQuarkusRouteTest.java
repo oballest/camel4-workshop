074f3385d90a7f0f3dc40d1dbaa5e706d7357d35
oballest <oballest@redhat.com>
2026-05-15 16:07:20 -0500

    plantilla para ejecución de lab6

--- camel-quarkus-starter/src/test/java/com/redhat/gps/tests/CamelQuarkusRouteTest.java
+++ camel-quarkus-starter/src/test/java/com/redhat/gps/tests/CamelQuarkusRouteTest.java
@@ -6,23 +6,12 @@ import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
 
-@QuarkusTest
+
 public class CamelQuarkusRouteTest extends CamelQuarkusTestSupport{
     
-    @Test
+
     void testRutaIntegración() throws InterruptedException{
-        MockEndpoint mockInicio = getMockEndpoint("mock:ingreso-ruta");
-        mockInicio.expectedBodiesReceived("Prueba Unitaria");
-        mockInicio.expectedMessageCount(1);
-
-        MockEndpoint mockFin = getMockEndpoint("mock:salida-ruta");
-        mockFin.expectedBodiesReceived("Prueba Unitaria-transformacion");
-        mockFin.expectedMessageCount(1);
-
-        template.sendBody("direct:ruta-prueba", "Prueba Unitaria");
-
-        mockInicio.assertIsSatisfied();
-        mockFin.assertIsSatisfied();
+      
     }
 
 }
