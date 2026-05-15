4db9993de7a83f30b7068a5221a97046f2ecadb1
oballest <oballest@redhat.com>
2026-05-15 13:19:11 -0500

    plantilla para laboratorio 4

--- camel-quarkus-starter/src/main/java/com/redhat/gps/routes/CamelSplitRouteBuilder.java
+++ camel-quarkus-starter/src/main/java/com/redhat/gps/routes/CamelSplitRouteBuilder.java
@@ -12,31 +12,7 @@ public class CamelSplitRouteBuilder extends RouteBuilder{
     @Override
     public void configure() throws Exception {
 
-        BindyCsvDataFormat bindy = new BindyCsvDataFormat(Persona.class);
-
-        from("file:../files/")
-        .log("Body del mensaje ${body}")
-        .split().tokenize("\n")
-        .log("nuevo body ${body}")
-        .unmarshal(bindy)
-        .log("Body en objetos ${body}")
-        .to("log:com.redhat.gps.routes.transform")
-        .marshal().json(JsonLibrary.Jackson)
-        .to("log:com.redhat.gps.routes.transform?showExchangeId=true")
-        .wireTap("direct:logArchivos")
-        .filter().jsonpath("$.[?(@.ciudad == 'Bogota' || @.ciudad == 'Medellin')]")
-        .log("Procesado mensaje filtrado por ciudad  ${body}")
-        .choice()
-            .when().jsonpath("$.[?(@.ciudad == 'Bogota')]")
-              .log("Mensaje Para Bogota")
-              .setHeader(Exchange.FILE_NAME).simple("${jsonpath($.nombre)}-${jsonpath($.apellido)}.json")
-              .to("file:out/Bogota/")
-            .when().jsonpath("$.[?(@.ciudad == 'Medellin')]")
-              .log("Mensaje para Medellin")
-              .setHeader(Exchange.FILE_NAME).simple("${jsonpath($.nombre)}-${jsonpath($.apellido)}.json")
-              .to("file:out/Medellin/")
-            .otherwise()
-              .log("Mensaje no reconocido");
+       
 
     }
     
