9dd06705ad9e7a99671860632fbf760ec85f10bf
oballest <oballest@redhat.com>
2026-05-15 13:08:39 -0500

    solución a laboratorio3

--- camel-quarkus-starter/src/main/java/com/redhat/gps/routes/CamelRouteBuilder.java
+++ camel-quarkus-starter/src/main/java/com/redhat/gps/routes/CamelRouteBuilder.java
@@ -7,15 +7,14 @@ public class CamelRouteBuilder extends RouteBuilder{
     @Override
     public void configure() throws Exception {
         
-        /*from("file:../files/?includeExt=csv")
-        .to("log:com.redhat.gps.routes.filereader?showAll=true")
-        .log("${body}")
-        .setBody(simple("${body}\n${header.CamelFileName}"))
-        .log("${body}")
-        .log("Fecha de modificación ${header.CamelFileLastModified} Ubicación del archivo ${header.CamelFilePath}")
-        .setHeader("CamelFileName").simple("${header.CamelFileName}-${header.CamelMessageTimestamp}")
-        .log("${header.CamelFileName}")
-        .to("file:out/");*/
+      from("file:../files/?includeExt=csv")
+      .to("log:com.redhat.gps.routes.filereader?showAll=true")
+      .setBody(simple("${body}\n${header.CamelFileName}"))
+      .log("${body}")
+      .log("Fecha de modificación ${header.CamelFileLastModified} Ubicación del archivo ${header.CamelFilePath}")
+      .setHeader("CamelFileName").simple("${header.CamelFileName}-${header.CamelMessageTimestamp}")
+      .log("${header.CamelFileName}")
+      .to("file:out/");
 
     }
     
